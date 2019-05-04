b57b2d93b29566442724674aed2953bc93b5ffe1
Autom <mijnnaamis2112@hotmail.com>
2019-05-04 18:50:21 +0200

    Apply standard formatting

--- index.js
+++ index.js
@@ -1,164 +1,161 @@
-const request = require('request-promise-native');
+const request = require('request-promise-native')
 
 const achievementTranslation = {
-    streak: 'wildfire',
-    completion: 'champion',
-    spending: 'bigSpender',
-    time: 'overtime',
-    social: 'friendly',
-    gold_skills: 'shiny',
-    xp: 'overachiever',
-    perfect: 'sharpshooter',
-};
+  streak: 'wildfire',
+  completion: 'champion',
+  spending: 'bigSpender',
+  time: 'overtime',
+  social: 'friendly',
+  gold_skills: 'shiny',
+  xp: 'overachiever',
+  perfect: 'sharpshooter'
+}
 const achievementDisplay = {
-    streak: 'Wildfire',
-    completion: 'Champion',
-    spending: 'Big Spender',
-    time: 'Overtime',
-    social: 'Friendly',
-    gold_skills: 'Shiny',
-    xp: 'Overachiever',
-    perfect: 'Sharpshooter',
-};
-const baseUrl = 'https://www.duolingo.com';
+  streak: 'Wildfire',
+  completion: 'Champion',
+  spending: 'Big Spender',
+  time: 'Overtime',
+  social: 'Friendly',
+  gold_skills: 'Shiny',
+  xp: 'Overachiever',
+  perfect: 'Sharpshooter'
+}
+const baseUrl = 'https://www.duolingo.com'
 
 let userCache = {
-    byNumber: {},
-    byName: {},
-};
+  byNumber: {},
+  byName: {}
+}
 
-function lookupInCache(identification) {
-    if (typeof(identification) === 'number') {
-        return userCache.byNumber[identification];
-    } else {
-        return userCache.byName[identification];
+function lookupInCache (identification) {
+  if (typeof (identification) === 'number') {
+    return userCache.byNumber[identification]
+  } else {
+    return userCache.byName[identification]
+  }
+}
+
+function addUserToCache (user) {
+  userCache.byNumber[user.id] = user
+  userCache.byName[user.username] = user
+}
+
+function requestDuolingo (endpoint) {
+  return request({
+    uri: baseUrl + endpoint,
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true
+  })
+}
+
+function arrayToObject (array, keyField) {
+  return array.reduce((obj, item) => {
+    obj[item[keyField]] = item
+    return obj
+  }, {})
+}
+
+function getSmallUser (id) {
+  return requestDuolingo('/2017-06-30/users/' + id)
+}
+function getLargeUser (username) {
+  return requestDuolingo('/users/' + username)
+}
+
+function User (rawSmallUser, rawLargeUser) {
+  this._rawSmallUser = rawSmallUser
+  this._rawLargeUser = rawLargeUser
+
+  this.id = rawLargeUser.id
+  this.username = rawLargeUser.username
+  this.duoname = this.username
+  this.avatarURL = 'https:' + rawLargeUser.avatar + '/xlarge'
+  this.originalLanguage = rawSmallUser.fromLanguage
+  this.hasPlus = rawSmallUser.hasPlus
+
+  this.courses = arrayToObject(rawSmallUser.courses, 'id')
+  this.courseId = rawSmallUser.currentCourseId
+  this.course = this.courses[this.courseId]
+
+  this.language = {}
+  this.language.learningCode = rawLargeUser['learning_language']
+  this.language.learningCodeUniversal = rawSmallUser.learningLanguage
+  this.language.learningString = rawLargeUser['learning_language_string']
+  this.language.learning = rawLargeUser['language_data'][this.language.learningCode]
+  this.language.level = this.language.learning.level
+  this.language.nextLevel = this.language.learning['next_level']
+  this.language.levelLeft = this.language.learning['level_left']
+  this.language.levelProgress = this.language.learning['level_progress']
+  this.language.levelPercent = this.language.learning['level_percent']
+  this.language.levelXP = this.language.learning['level_points']
+  this.language.totalXP = this.language.learning.points
+  this.language.skillsLearned = this.language.learning['num_skills_learned']
+  this.language.totalSkills = this.language.learning.skills.length
+  this.language.crowns = this.course.crowns
+
+  this.streak = {}
+  this.streak.currentAmount = this.language.learning.streak
+  this.streak.extendedToday = rawLargeUser['streak_extended_today']
+
+  this.achievements = {}
+  rawSmallUser['_achievements'].forEach(achievement => {
+    let translated = achievementTranslation[achievement.name]
+    if (!translated) {
+      return
     }
-}
+    this.achievements[translated] = {
+      tier: achievement.tier,
+      count: achievement.count,
+      tierCounts: achievement.tierCounts,
+      name: achievement.name,
+      displayName: achievementDisplay[achievement.name],
+      nextTierCount: achievement.tierCounts[achievement.tier - 1],
+      maxedOut: achievement.count >= achievement.tierCounts[achievement.tierCounts.length - 1]
+    }
+  })
 
-function addUserToCache(user) {
-    userCache.byNumber[user.id] = user;
-    userCache.byName[user.username] = user;
-}
-
-function requestDuolingo(endpoint) {
-    return request({
-        uri: baseUrl + endpoint,
-        headers: {
-            'User-Agent': 'Request-Promise'
-        },
-        json: true,
-    });
-}
-
-function arrayToObject(array, keyField) {
-    return array.reduce((obj, item) => {
-        obj[item[keyField]] = item;
-        return obj;
-    }, {});
-}
-
-function getSmallUser(id) {
-    return requestDuolingo('/2017-06-30/users/' + id);
-}
-function getLargeUser(username) {
-    return requestDuolingo('/users/' + username);
-}
-
-function User(rawSmallUser, rawLargeUser) {
-    this._rawSmallUser = rawSmallUser;
-    this._rawLargeUser = rawLargeUser;
-
-    this.id = rawLargeUser.id;
-    this.username = rawLargeUser.username;
-    this.duoname = this.username;
-    this.avatarURL = 'https:' + rawLargeUser.avatar + '/xlarge';
-    this.originalLanguage = rawSmallUser.fromLanguage;
-    this.hasPlus = rawSmallUser.hasPlus;
-
-    this.courses = arrayToObject(rawSmallUser.courses, 'id');
-    this.courseId = rawSmallUser.currentCourseId;
-    this.course = this.courses[this.courseId];
-
-    this.language = {};
-    this.language.learningCode = rawLargeUser['learning_language'];
-    this.language.learningCodeUniversal = rawSmallUser.learningLanguage;
-    this.language.learningString = rawLargeUser['learning_language_string'];
-    this.language.learning = rawLargeUser['language_data'][this.language.learningCode];
-    this.language.level = this.language.learning.level;
-    this.language.nextLevel = this.language.learning['next_level'];
-    this.language.levelLeft = this.language.learning['level_left'];
-    this.language.levelProgress = this.language.learning['level_progress'];
-    this.language.levelPercent = this.language.learning['level_percent'];
-    this.language.levelXP = this.language.learning['level_points'];
-    this.language.totalXP = this.language.learning.points;
-    this.language.skillsLearned = this.language.learning['num_skills_learned'];
-    this.language.totalSkills = this.language.learning.skills.length;
-    this.language.crowns = this.course.crowns;
-
-    this.streak = {};
-    this.streak.currentAmount = this.language.learning.streak;
-    this.streak.extendedToday = rawLargeUser['streak_extended_today'];
-
-    this.achievements = {};
-    rawSmallUser['_achievements'].forEach(achievement => {
-        let translated = achievementTranslation[achievement.name];
-        if (!translated) {
-            return;
-        }
-        this.achievements[translated] = {
-            tier: achievement.tier,
-            count: achievement.count,
-            tierCounts: achievement.tierCounts,
-            name: achievement.name,
-            displayName: achievementDisplay[achievement.name],
-            nextTierCount: achievement.tierCounts[achievement.tier - 1],
-            maxedOut: achievement.count >= achievement.tierCounts[achievement.tierCounts.length - 1],
-        };
-    });
-
-	/*this.getName = function () {
-		return this.name;
-	};*/
-    addUserToCache(this);
+  addUserToCache(this)
 }
 
 module.exports = function (identification) {
-    let user = lookupInCache(identification);
-    if (user) {
-        return new Promise((resolve, reject) => resolve(user));
-    }
-    return new Promise((resolve, reject) => {
-        let rawSmallUser = null;
-        let rawLargeUser = null;
+  let user = lookupInCache(identification)
+  if (user) {
+    return new Promise((resolve, reject) => resolve(user))
+  }
+  return new Promise((resolve, reject) => {
+    let rawSmallUser = null
+    let rawLargeUser = null
 
-        if (typeof(identification) === 'number') {
-            getSmallUser(identification)
-                .then((rawSmallUser) => {
-                    getLargeUser(rawSmallUser.username)
-                        .then((rawLargeUser) => {
-                            resolve(new User(rawSmallUser, rawLargeUser));
-                        })
-                        .catch((error) => {
-                            reject(error);
-                        });
-                })
-                .catch((error) => {
-                    reject(error);
-                });
-        } else {
-            getLargeUser(identification)
-                .then((rawLargeUser) => {
-                    getSmallUser(rawLargeUser.id)
-                        .then((rawSmallUser) => {
-                            resolve(new User(rawSmallUser, rawLargeUser));
-                        })
-                        .catch((error) => {
-                            reject(error);
-                        });
-                })
-                .catch((error) => {
-                    reject(error);
-                });
-        }
-    });
-};
+    if (typeof (identification) === 'number') {
+      getSmallUser(identification)
+        .then((rawSmallUser) => {
+          getLargeUser(rawSmallUser.username)
+            .then((rawLargeUser) => {
+              resolve(new User(rawSmallUser, rawLargeUser))
+            })
+            .catch((error) => {
+              reject(error)
+            })
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    } else {
+      getLargeUser(identification)
+        .then((rawLargeUser) => {
+          getSmallUser(rawLargeUser.id)
+            .then((rawSmallUser) => {
+              resolve(new User(rawSmallUser, rawLargeUser))
+            })
+            .catch((error) => {
+              reject(error)
+            })
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    }
+  })
+}
--- test.js
+++ test.js
@@ -1,84 +1,84 @@
-const duo = require('./index.js');
+const duo = require('./index.js')
 
-let firstStartTime = new Date();
+let firstStartTime = new Date()
 duo('HelpfulDuo')
-    .then(function(user) {
-        let firstEndTime = new Date();
-        if (user.id === 98212660) {
-            console.log('User id is correct');
-        } else {
-            console.log('User id is wrong: ' + user.id);
-        }
-        if (user.username === 'HelpfulDuo') {
-            console.log('User username is correct');
-        } else {
-            console.log('User username is wrong: ' + user.username);
-        }
-        if (user.duoname === 'HelpfulDuo') {
-            console.log('User duoname is correct');
-        } else {
-            console.log('User duoname is wrong: ' + user.duoname);
-        }
-        if (user.avatarURL === 'https://duolingo-images.s3.amazonaws.com/avatars/98212660/qA51lqRF1i/xlarge') {
-            console.log('User avatarURL is correct');
-        } else {
-            console.log('User avatarURL is wrong: ' + user.avatarURL);
-        }
-        if (user.language.learningCode === 'fr') {
-            console.log('User language learningCode is correct');
-        } else {
-            console.log('User language learningCode is wrong: ' + user.language.learningCode);
-        }
-        if (user.language.learningString === 'French') {
-            console.log('User language learningString is correct');
-        } else {
-            console.log('User language learningString is wrong: ' + user.language.learningString);
-        }
-        if (user.language.level === 1) {
-            console.log('User language level is correct');
-        } else {
-            console.log('User language level is wrong: ' + user.language.level);
-        }
-        if (user.language.skillsLearned === 1) {
-            console.log('User language skillsLearned is correct');
-        } else {
-            console.log('User language skillsLearned is wrong: ' + user.language.skillsLearned);
-        }
-        if (user.language.totalSkills === 78) {
-            console.log('User language totalSkills is correct');
-        } else {
-            console.log('User language totalSkills is wrong: ' + user.language.totalSkills);
-        }
-        if (user.streak.currentAmount === 0) {
-            console.log('User streak.currentAmount is correct');
-        } else {
-            console.log('User streak.currentAmount is wrong: ' + user.streak.currentAmount);
-        }
-        if (user.streak.extendedToday === false) {
-            console.log('User streak extendedToday is correct');
-        } else {
-            console.log('User streak extendedToday is wrong: ' + user.streak.extendedToday);
-        }
-        if (user.achievements.bigSpender.maxedOut === false) {
-            console.log('User achievements bigSpender maxedOut is correct');
-        } else {
-            console.log('User achievements bigSpender maxedOut is wrong: ' + user.achievements.bigSpender.maxedOut);
-        }
-        if (user.achievements.shiny.tier === 2) {
-            console.log('User achievements shiny tier is correct');
-        } else {
-            console.log('User achievements shiny tier is wrong: ' + user.achievements.shiny.tier);
-        }
-        let secondStartTime = new Date();
-        duo('HelpfulDuo').then(function(user) {
-            let secondEndTime = new Date();
-            let firstDelta = firstEndTime.getTime() - firstStartTime.getTime();
-            let secondDelta = secondEndTime.getTime() - secondStartTime.getTime();
-            console.log('Timing before cache: ' + firstDelta + 'ms');
-            console.log('Timing after cache: ' + secondDelta + 'ms');
-            console.log('Delta timgins check ' + (secondDelta < firstDelta ? 'passed' : 'failed'));
-        });
+  .then(function (user) {
+    let firstEndTime = new Date()
+    if (user.id === 98212660) {
+      console.log('User id is correct')
+    } else {
+      console.log('User id is wrong: ' + user.id)
+    }
+    if (user.username === 'HelpfulDuo') {
+      console.log('User username is correct')
+    } else {
+      console.log('User username is wrong: ' + user.username)
+    }
+    if (user.duoname === 'HelpfulDuo') {
+      console.log('User duoname is correct')
+    } else {
+      console.log('User duoname is wrong: ' + user.duoname)
+    }
+    if (user.avatarURL === 'https://duolingo-images.s3.amazonaws.com/avatars/98212660/qA51lqRF1i/xlarge') {
+      console.log('User avatarURL is correct')
+    } else {
+      console.log('User avatarURL is wrong: ' + user.avatarURL)
+    }
+    if (user.language.learningCode === 'fr') {
+      console.log('User language learningCode is correct')
+    } else {
+      console.log('User language learningCode is wrong: ' + user.language.learningCode)
+    }
+    if (user.language.learningString === 'French') {
+      console.log('User language learningString is correct')
+    } else {
+      console.log('User language learningString is wrong: ' + user.language.learningString)
+    }
+    if (user.language.level === 1) {
+      console.log('User language level is correct')
+    } else {
+      console.log('User language level is wrong: ' + user.language.level)
+    }
+    if (user.language.skillsLearned === 1) {
+      console.log('User language skillsLearned is correct')
+    } else {
+      console.log('User language skillsLearned is wrong: ' + user.language.skillsLearned)
+    }
+    if (user.language.totalSkills === 78) {
+      console.log('User language totalSkills is correct')
+    } else {
+      console.log('User language totalSkills is wrong: ' + user.language.totalSkills)
+    }
+    if (user.streak.currentAmount === 0) {
+      console.log('User streak.currentAmount is correct')
+    } else {
+      console.log('User streak.currentAmount is wrong: ' + user.streak.currentAmount)
+    }
+    if (user.streak.extendedToday === false) {
+      console.log('User streak extendedToday is correct')
+    } else {
+      console.log('User streak extendedToday is wrong: ' + user.streak.extendedToday)
+    }
+    if (user.achievements.bigSpender.maxedOut === false) {
+      console.log('User achievements bigSpender maxedOut is correct')
+    } else {
+      console.log('User achievements bigSpender maxedOut is wrong: ' + user.achievements.bigSpender.maxedOut)
+    }
+    if (user.achievements.shiny.tier === 2) {
+      console.log('User achievements shiny tier is correct')
+    } else {
+      console.log('User achievements shiny tier is wrong: ' + user.achievements.shiny.tier)
+    }
+    let secondStartTime = new Date()
+    duo('HelpfulDuo').then(function (user) {
+      let secondEndTime = new Date()
+      let firstDelta = firstEndTime.getTime() - firstStartTime.getTime()
+      let secondDelta = secondEndTime.getTime() - secondStartTime.getTime()
+      console.log('Timing before cache: ' + firstDelta + 'ms')
+      console.log('Timing after cache: ' + secondDelta + 'ms')
+      console.log('Delta timgins check ' + (secondDelta < firstDelta ? 'passed' : 'failed'))
     })
-    .catch(function(error) {
-        console.error(error);
-    });
+  })
+  .catch(function (error) {
+    console.error(error)
+  })
